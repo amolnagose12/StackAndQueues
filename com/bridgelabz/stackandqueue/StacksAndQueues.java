89d539ab986c1fa8733072a573fd899f966bff71
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-11-16 12:52:32 +0530

    Ability to peak and

---
+++ com/bridgelabz/stackandqueue/StacksAndQueues.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.stackandqueue;
+
+public class StacksAndQueues {
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+        System.out.println("The top element is " + stack.peek());
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        if (stack.isEmpty()) {
+            System.out.print("The stack is empty");
+        } else {
+            System.out.print("The stack is not empty");
+        }
+    }
+}
